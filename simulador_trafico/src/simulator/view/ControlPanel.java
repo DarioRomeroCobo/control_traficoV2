81381e617e14ea0de64cdc870a9f375177dd113b
adria <adria@PC-del-admin>
2025-03-24 22:22:48 +0100

    solo sé que no sé nada

--- simulador_trafico/src/simulator/view/ControlPanel.java
+++ simulador_trafico/src/simulator/view/ControlPanel.java
@@ -4,6 +4,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.util.Collection;
 
 import javax.swing.*;
@@ -14,64 +15,77 @@ import simulator.model.RoadMap;
 import simulator.model.TrafficSimObserver;
 
 public class ControlPanel extends JPanel implements TrafficSimObserver {
-	
-	private Controller _ctrl;
-	
-	ControlPanel(Controller ctrl){
-		this._ctrl = ctrl;
-		
-		  JButton openButton = new JButton("Abrir fichero de eventos");
 
-	        openButton.addActionListener(new ActionListener() {
-	        	
-	            @Override
-	            public void actionPerformed(ActionEvent e) {
-	            
-	                JFileChooser fileChooser = new JFileChooser();
-	                
-	                File file=fileChooser.getSelectedFile();
-	                
-	                try (FileInputStream fis = new FileInputStream(file)){
-	                
-	                		
-	                _ctrl.reset();
-	                _ctrl.loadEvents(fis);
-	                	
-	                	
-	                	
-	                }catch(IllegalArgumentException ex) {
-	                	JOptionPane.showMessageDialog(openButton, ex.getMessage());
-	                	
-	                }
-	            }
-	        }
+	private Controller _ctrl;
+
+	ControlPanel(Controller ctrl) {
+		this._ctrl = ctrl;
+		initGui();
+
 	}
 
+	private void initGui(){
+		
+		JButton fileButton = new JButton("Abrir fichero de eventos");
+
+		fileButton.addActionListener(new ActionListener() {
+        	
+            @Override
+            public void actionPerformed(ActionEvent e) {
+         
+                JFileChooser fileChooser = new JFileChooser();
+                
+                if (fileChooser.showOpenDialog(null)==JFileChooser.APPROVE_OPTION) {
+                	 File file = fileChooser.getSelectedFile();
+                
+                	 try( FileInputStream input = new FileInputStream(file)){
+                		
+                		 _ctrl.reset();
+                		 _ctrl.loadEvents(input);
+                	 }
+                	 
+                	 catch(IllegalArgumentException ex) {
+                			JOptionPane.showMessageDialog(fileButton, ex.getMessage());
+                	
+                	 } catch (FileNotFoundException f) {
+						
+                		 JOptionPane.showMessageDialog(fileButton, f.getMessage());
+					}
+                	 catch (Exception ex) {
+                         JOptionPane.showMessageDialog(fileButton, "Error inesperado: " + ex.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+                     }
+                	 
+                	 
+                }
+                
+            }
 	
-	
-	
+        });
+        this.add(fileButton);
+	}
+
 	@Override
 	public void onAdvance(RoadMap map, Collection<Event> events, int time) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void onEventAdded(RoadMap map, Collection<Event> events, Event e, int time) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void onReset(RoadMap map, Collection<Event> events, int time) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void onRegister(RoadMap map, Collection<Event> events, int time) {
 		// TODO Auto-generated method stub
-		
+
 	}
-	
+
 }
