a01885afea5876dd4b31419ac82e56fce838417c
adria <adria@PC-del-admin>
2025-03-29 14:51:29 +0100

    JToolBar hecho 🤓☝

--- simulador_trafico/src/simulator/view/ChangeWeatherDialog.java
+++ simulador_trafico/src/simulator/view/ChangeWeatherDialog.java
@@ -1,7 +1,105 @@
 package simulator.view;
 
-import javax.swing.JDialog;
+import java.awt.Frame;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.List;
 
-public class ChangeWeatherDialog extends JDialog{
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JDialog;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JSpinner;
+import javax.swing.SpinnerNumberModel;
+
+import simulator.control.Controller;
+import simulator.misc.Pair;
+import simulator.model.Road;
+import simulator.model.SetContClassEvent;
+import simulator.model.SetWeatherEvent;
+import simulator.model.Vehicle;
+import simulator.model.Weather;
+
+public class ChangeWeatherDialog extends JDialog {
+
+	private Controller _ctrl;
+	private List<Road> roads;
+	private int currTime;
+
+	public ChangeWeatherDialog(Frame parent, Controller _ctrl, List<Road> roads, int currTime) {
+		super(parent, "Change Road Weather", true);
+		this._ctrl = _ctrl;
+		this.roads = roads;
+		this.currTime = currTime;
+		initGUI();
+	}
+
+	private void initGUI() {
+		this.setLayout(new BoxLayout(getContentPane(), BoxLayout.Y_AXIS));
+
+		JLabel message = new JLabel(
+				"Schedule an event to change the weather of a road after a given number of simulation ticks from now.");
+		message.setAlignmentX(CENTER_ALIGNMENT);
+		this.add(message);
+		this.add(Box.createVerticalStrut(10));
+
+		JPanel panelRoad = new JPanel();
+		panelRoad.setLayout(new BoxLayout(panelRoad, BoxLayout.X_AXIS));
+
+		JComboBox<Vehicle> rList = new JComboBox<>(roads.toArray(new Vehicle[0]));
+		rList.setSelectedIndex(0);
+
+		panelRoad.add(new JLabel("Road: "));
+		panelRoad.add(rList);
+		this.add(panelRoad);
+		this.add(Box.createVerticalStrut(5));
+
+		JPanel panelWeather = new JPanel();
+		panelWeather.setLayout(new BoxLayout(panelWeather, BoxLayout.X_AXIS));
+
+		JComboBox<Weather> weatherList = new JComboBox<>(Weather.values());
+
+		panelWeather.add(new JLabel("Weather: "));
+		panelWeather.add(weatherList);
+		this.add(panelWeather);
+		this.add(Box.createVerticalStrut(5));
+
+		JPanel panelTicks = new JPanel();
+		panelTicks.setLayout(new BoxLayout(panelTicks, BoxLayout.X_AXIS));
+
+		JSpinner spinnerTicks = new JSpinner(new SpinnerNumberModel(1, 1, 100, 1));
+
+		panelTicks.add(new JLabel("Ticks: "));
+		panelTicks.add(spinnerTicks);
+		this.add(panelTicks);
+		this.add(Box.createVerticalStrut(10));
+
+		JPanel panelButtons = new JPanel();
+		panelButtons.setLayout(new BoxLayout(panelButtons, BoxLayout.X_AXIS));
+
+		JButton cancelButton = new JButton("Cancel");
+		cancelButton.addActionListener(e -> setVisible(false));
+
+		JButton okButton = new JButton("OK");
+		okButton.addActionListener(new ActionListener() {
+
+			public void actionPerformed(ActionEvent e) {
+				List<Pair<String, Weather>> si = new ArrayList<>();
+				si.add(new Pair<>(rList.getSelectedItem().toString(), (Weather) weatherList.getSelectedItem()));
+				_ctrl.addEvent(new SetWeatherEvent((int) spinnerTicks.getValue() + currTime, si));
+				setVisible(false);
+			}
+		});
+
+		panelButtons.add(cancelButton);
+		panelButtons.add(Box.createHorizontalStrut(10));
+		panelButtons.add(okButton);
+		this.add(panelButtons);
+
+	}
 
 }
--- simulador_trafico/src/simulator/view/ControlPanel.java
+++ simulador_trafico/src/simulator/view/ControlPanel.java
@@ -1,16 +1,20 @@
 package simulator.view;
 
+import java.awt.Frame;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 import javax.swing.*;
 
 import simulator.control.Controller;
 import simulator.model.Event;
+import simulator.model.Road;
 import simulator.model.RoadMap;
 import simulator.model.TrafficSimObserver;
 
@@ -20,21 +24,23 @@ public class ControlPanel extends JPanel implements TrafficSimObserver {
 	private RoadMap map;
 	private Collection<Event> events;
 	private int time;
+	private boolean _stopped;
+	private List<JButton> buttons;
 
 	ControlPanel(Controller ctrl) {
 		this._ctrl = ctrl;
 		ctrl.addObserver(this);
+		_stopped = true;
+		buttons = new ArrayList<>();
 		initGui();
 
-		_ctrl.addEvent(null);
 	}
 
 	private void initGui() {
+
 		JToolBar toolBar = new JToolBar();
 
 		JButton fileButton = new JButton(new ImageIcon("resources/icons/open.png"));
-
-		toolBar.add(fileButton);
 
 		fileButton.addActionListener(new ActionListener() {
 
@@ -69,53 +75,130 @@ public class ControlPanel extends JPanel implements TrafficSimObserver {
 
 		});
 
-		JButton CO2Button = new JButton(new ImageIcon("resources/icons/co2class.png"));
-		CO2Button.addActionListener(new ActionListener() {
+		toolBar.add(fileButton);
+
+		JButton co2Button = new JButton(new ImageIcon("resources/icons/co2class.png"));
+		co2Button.addActionListener(new ActionListener() {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
 
-				ChangeCO2ClassDialog dialog = new ChangeCO2ClassDialog(null, _ctrl, map.getVehicles(),time);
+				ChangeCO2ClassDialog dialog = new ChangeCO2ClassDialog(null, _ctrl, map.getVehicles(), time);
+				dialog.setVisible(true);
+			}
+
+		});
+		toolBar.add(co2Button);
+		JButton weatherButton = new JButton(new ImageIcon("resources/icons/weather.png"));
+		weatherButton.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				ChangeWeatherDialog dialog = new ChangeWeatherDialog(null, _ctrl, map.getRoads(), time);
 				dialog.setVisible(true);
 			}
 
 		});
 
-		toolBar.add(CO2Button);
+		toolBar.add(weatherButton);
+
+		JPanel panelTicks = new JPanel();
+		panelTicks.setLayout(new BoxLayout(panelTicks, BoxLayout.X_AXIS));
+		JSpinner spinnerTicks = new JSpinner(new SpinnerNumberModel(1, 1, 100, 1));
+		panelTicks.add(new JLabel("Ticks:"));
+		panelTicks.add(spinnerTicks);
+
+		JButton runButton = new JButton(new ImageIcon("resources/icons/run.png"));
+		runButton.addActionListener(e -> {
+			_stopped = false;
+			enableToolbar();
+			run_sim((int) spinnerTicks.getValue());
+		});
+
+		JButton stopButton = new JButton(new ImageIcon("resources/icons/stop.png"));
+		stopButton.addActionListener(e -> {
+			this._stopped = true;
+		});
+
+		JButton exitButton = new JButton(new ImageIcon("resources/icons/exit.png"));
+
+		exitButton.addActionListener(e -> {
+			int response = JOptionPane.showConfirmDialog(null, "Do you want to exit the program?", "Exit",
+					JOptionPane.YES_NO_OPTION);
+
+			if (response == JOptionPane.YES_OPTION)
+				System.exit(0);
+
+		});
+
+		toolBar.add(runButton);
+		toolBar.add(stopButton);
+		toolBar.add(panelTicks);
+		
+		toolBar.add(Box.createHorizontalGlue());
+		toolBar.add(exitButton);
+
+		buttons.add(fileButton);
+		buttons.add(co2Button);
+		buttons.add(weatherButton);
+		buttons.add(runButton);
+		buttons.add(exitButton);
 
 		this.add(toolBar);
 	}
 
-	public void update(RoadMap map,Collection<Event> events, int time) {
+	private void enableToolbar() {
+		for (JButton b : buttons)
+			b.setEnabled(this._stopped);
+	}
+
+	private void run_sim(int n) {
+		if (n > 0 && !_stopped) {
+			try {
+				_ctrl.run(1);
+				SwingUtilities.invokeLater(() -> run_sim(n - 1));
+			} catch (Exception e) {
+				JOptionPane.showMessageDialog(null, "Error at execute");
+
+				_stopped = true;
+				enableToolbar();
+			}
+		} else {
+			_stopped = true;
+			enableToolbar();
+		}
+	}
+
+	public void update(RoadMap map, Collection<Event> events, int time) {
 		SwingUtilities.invokeLater(() -> {
 			this.map = map;
-			this.events=events;
-			this.time=time;
+			this.events = events;
+			this.time = time;
 			repaint();
 		});
 	}
 
 	@Override
 	public void onAdvance(RoadMap map, Collection<Event> events, int time) {
-		update(map);
+		update(map, events, time);
 
 	}
 
 	@Override
 	public void onEventAdded(RoadMap map, Collection<Event> events, Event e, int time) {
-		update(map);
+		update(map, events, time);
 
 	}
 
 	@Override
 	public void onReset(RoadMap map, Collection<Event> events, int time) {
-		update(map);
+		update(map, events, time);
 
 	}
 
 	@Override
 	public void onRegister(RoadMap map, Collection<Event> events, int time) {
-		update(map);
+		update(map, events, time);
 	}
 
 }
